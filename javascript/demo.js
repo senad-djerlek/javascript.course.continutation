f4ad17455eee73d3bfb0fcd3a65e4317ce0b7558
Senad Djerlek <senajerlek@gmail.com>
2022-06-06 17:57:12 +0200

    javascript exercise done

--- javascript/demo.js
+++ javascript/demo.js
@@ -1,15 +1,15 @@
-let productNameInputElement = document.getElementById("product-name");
-let remainingCharsElement = document.getElementById("remaining-chars");
+const productNameInputElement = document.getElementById("product-name");
+const remainingCharsElement = document.getElementById("remaining-chars");
 
 // console.dir(productNameInputElement);//za proveru
 
-let maxAllowedChars = productNameInputElement.maxLength;
+const maxAllowedChars = productNameInputElement.maxLength;
 
 function updateRemainginCharacters(event) {
-    let enteredText = event.target.value;
-    let enteredTextLength = enteredText.length;
+    const enteredText = event.target.value;
+    const enteredTextLength = enteredText.length;
 
-    let remainingCharacters = maxAllowedChars - enteredTextLength;
+    const remainingCharacters = maxAllowedChars - enteredTextLength;
 
     remainingCharsElement.textContent = remainingCharacters;
 }
