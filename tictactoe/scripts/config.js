32f697cde0fae30d12817e0794ea46eec042809d
Senad Djerlek <senajerlek@gmail.com>
2022-06-25 19:20:48 +0200

    adding logic

--- tictactoe/scripts/config.js
+++ tictactoe/scripts/config.js
@@ -1,16 +1,24 @@
-function openPlayerConfig () {
-    playerConfigOverlayElement.style.display = "block"
-    backDropElement.style.display = "block"
+function openPlayerConfig() {
+  playerConfigOverlayElement.style.display = "block";
+  backDropElement.style.display = "block";
 }
 
-function closePlayerConfig () {
-    playerConfigOverlayElement.style.display = "none"
-    backDropElement.style.display = "none"
+function closePlayerConfig() {
+  playerConfigOverlayElement.style.display = "none";
+  backDropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutputElement.textContent = '';
 }
 
-function savePlayerConfig (event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const enteredPlayerName = formData.get("playername");
-    console.log(enteredPlayerName);
+function savePlayerConfig(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayerName = formData.get("playername").trim(); //trim gets rid of whitespace of the actual content
+
+  if (!enteredPlayerName) {
+    event.target.firstElementChild.classList.add("error")
+    errorsOutputElement.textContent = "Please enter a valid name!";
+    return;
+  }
+
 }
