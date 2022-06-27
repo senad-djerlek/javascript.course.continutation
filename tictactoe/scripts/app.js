325b5e23aca7da274a328137114836aa25cbc488
Senad Djerlek <senajerlek@gmail.com>
2022-06-27 20:46:07 +0200

    js p

--- tictactoe/scripts/app.js
+++ tictactoe/scripts/app.js
@@ -1,3 +1,16 @@
+let editedPlayer = 0;
+
+const players = [
+    {
+        name: "",
+        symbol : "X"
+    },
+    {
+        name: "",
+        symbol: "O"
+    },
+];
+
 const playerConfigOverlayElement = document.getElementById("config-overlay");
 const backDropElement = document.getElementById("backdrop");
 const formElement = document.querySelector("form");
--- tictactoe/scripts/config.js
+++ tictactoe/scripts/config.js
@@ -1,15 +1,15 @@
-function openPlayerConfig() {
+function openPlayerConfig(event) {
+  editedPlayer = +event.target.dataset.playerid;
   playerConfigOverlayElement.style.display = "block";
   backDropElement.style.display = "block";
 }
-
 function closePlayerConfig() {
   playerConfigOverlayElement.style.display = "none";
   backDropElement.style.display = "none";
   formElement.firstElementChild.classList.remove("error");
   errorsOutputElement.textContent = '';
+  formElement.firstElementChild.lastElementChild.value = '';
 }
-
 function savePlayerConfig(event) {
   event.preventDefault();
   const formData = new FormData(event.target);
@@ -20,5 +20,10 @@ function savePlayerConfig(event) {
     errorsOutputElement.textContent = "Please enter a valid name!";
     return;
   }
+  const updatedPlayerDataElement = document.getElementById("player-" + editedPlayer + "-data");
+  updatedPlayerDataElement.children[1].textContent = enteredPlayerName;
 
+  players[editedPlayer - 1].name = enteredPlayerName;
+
+  closePlayerConfig();
 }
