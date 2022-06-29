5e110a5e2c82cda22e271a4e15e6bdcc66f028f0
Senad Djerlek <senajerlek@gmail.com>
2022-06-29 18:52:51 +0200

    js p

--- tictactoe/scripts/app.js
+++ tictactoe/scripts/app.js
@@ -15,10 +15,13 @@ const playerConfigOverlayElement = document.getElementById("config-overlay");
 const backDropElement = document.getElementById("backdrop");
 const formElement = document.querySelector("form");
 const errorsOutputElement = document.getElementById("config-errors")
+const gameAreaElement = document.getElementById("active-game");
 
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
 const cancelConfigBtnElement = document.getElementById("cancel-config-btn");
+const startNewGameBtnElement = document.getElementById("start-game-btn");
+
 
 editPlayer1BtnElement.addEventListener("click", openPlayerConfig);
 editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
@@ -27,4 +30,6 @@ cancelConfigBtnElement.addEventListener("click", closePlayerConfig);
 
 backDropElement.addEventListener("click", closePlayerConfig);
 
-formElement.addEventListener("submit",savePlayerConfig)
+formElement.addEventListener("submit",savePlayerConfig);
+
+startNewGameBtnElement.addEventListener("click", startNewGame);
