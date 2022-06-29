6782de68b540c95dbfefa674405c37f785060dfd
Senad Djerlek <senajerlek@gmail.com>
2022-06-29 19:04:39 +0200

    js p

--- tictactoe/scripts/app.js
+++ tictactoe/scripts/app.js
@@ -1,4 +1,5 @@
 let editedPlayer = 0;
+let activePlayer = 0;
 
 const players = [
     {
@@ -17,10 +18,13 @@ const formElement = document.querySelector("form");
 const errorsOutputElement = document.getElementById("config-errors")
 const gameAreaElement = document.getElementById("active-game");
 
+
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
 const cancelConfigBtnElement = document.getElementById("cancel-config-btn");
 const startNewGameBtnElement = document.getElementById("start-game-btn");
+const gameFiledElements = document.querySelectorAll("#game-board li");
+
 
 
 editPlayer1BtnElement.addEventListener("click", openPlayerConfig);
@@ -33,3 +37,7 @@ backDropElement.addEventListener("click", closePlayerConfig);
 formElement.addEventListener("submit",savePlayerConfig);
 
 startNewGameBtnElement.addEventListener("click", startNewGame);
+
+for (const gameFiledElement of gameFiledElements) {
+    gameFiledElement.addEventListener("click", selectGameField);
+}
--- tictactoe/scripts/game.js
+++ tictactoe/scripts/game.js
@@ -1,7 +1,23 @@
 function startNewGame () {
+    
+    gameAreaElement.style.display = "block";
     if (players[0].name===""|| player[1].name === ""){
         alert("Please set custom player names for both players!")
         return;
     }
-    gameAreaElement.style.display = "block";
+    // gameAreaElement.style.display = "block";
+}
+
+function switchPlayer (){
+    if(activePlayer === 0) {
+        activePlayer = 1;
+    } else {
+        activePlayer = 0;
+    }
+}
+
+function selectGameField (event) {
+    event.target.textContent = players[activePlayer].symbol;  
+    event.target.classList.add("disabled");
+    switchPlayer();
 }
