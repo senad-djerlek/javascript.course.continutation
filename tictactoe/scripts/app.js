a0a49a3cb18f4b135225f616382e0cb4b7a87d99
Senad Djerlek <senajerlek@gmail.com>
2022-07-31 18:44:41 +0200

    js practice

--- tictactoe/scripts/app.js
+++ tictactoe/scripts/app.js
@@ -7,6 +7,7 @@ const gameData = [
 
 let editedPlayer = 0;
 let activePlayer = 0;
+let currentRound = 1;
 
 const players = [
     {
@@ -25,7 +26,7 @@ const formElement = document.querySelector("form");
 const errorsOutputElement = document.getElementById("config-errors")
 const gameAreaElement = document.getElementById("active-game");
 const activePlayerNameElement = document.getElementById("active-player-name");
-
+const gameOverElement = document.getElementById("game-over")
 
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
